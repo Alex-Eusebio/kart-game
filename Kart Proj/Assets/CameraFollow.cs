4df623c08adbea523bbcab592572de3d2d12b23a
gig <alexandreeusebio499p@gmail.com>
2024-09-18 16:55:44 +0100

    First Commit
    Car system
    Camera follow car
    Drfting + Boost

---
+++ Kart Proj/Assets/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+    [SerializeField]
+    private Transform camPos;
+    [SerializeField]
+    private Transform camPos2;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = this.gameObject;
+    }
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        _camera.transform.position = Vector3.Lerp(camPos2.position, camPos.position, Time.deltaTime*player.GetComponent<CarSystem>().currentSpeed);
+        _camera.transform.LookAt(player.gameObject.transform.position);
+    }
+}
