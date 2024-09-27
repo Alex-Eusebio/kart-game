f28667519c82f0e62c072287af2fd2e01765fe3a
gig <115169858+Alex-Eusebio@users.noreply.github.com>
2024-09-27 16:50:23 +0100

    Adjusts and Terrain Speed Changes
    -Added booster pads
    -Terrain now can apply speed changes (buffs and debuffs)
    -Adjusted steering to be highter, but the value has less power when drifting (customizable)
    -Lowered the passive steering when drifting (also its customizable now)

---
+++ Kart Proj/Assets/Code/Floor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Floor : MonoBehaviour, IChangeSpeed
+{
+    [SerializeField]
+    float speedChange;
+    [SerializeField]
+    float maxTimer;
+    float timer;
+
+    public float ChangeSpeed(float speed)
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxTimer && speed > 10)
+        {
+            timer = 0;
+            return speedChange;
+        }
+        return 0;
+    }
+}
