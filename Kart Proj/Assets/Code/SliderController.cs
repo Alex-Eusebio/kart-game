d563227ac1d802653ef72efb4c6f0bf3654dd9cd
Richardgold00 <Ricardo5050@live.com>
2024-11-21 21:02:32 +0000

    Fix de um bug com os Sliders

--- Kart Proj/Assets/Code/SliderController.cs
+++ Kart Proj/Assets/Code/SliderController.cs
@@ -39,16 +39,37 @@ public class SliderController : MonoBehaviour
         Handling.maxValue = maxValue;
     }
 
+    void Update()
+    {
+        // Verifica a entrada das teclas A e D ou as setas para controlar a mudança de personagem
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && currentCharacterIndex > 0)
+        {
+            OnPreviousButtonClicked();
+        }
+        else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && currentCharacterIndex < characters.Length - 1)
+        {
+            OnNextButtonClicked();
+        }
+    }
+
     public void OnNextButtonClicked()
     {
-        currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
-        UpdateSliders();
+        // Avança para o próximo personagem se não for o último
+        if (currentCharacterIndex < characters.Length - 1)
+        {
+            currentCharacterIndex++;
+            UpdateSliders();
+        }
     }
 
     public void OnPreviousButtonClicked()
     {
-        currentCharacterIndex = (currentCharacterIndex - 1 + characters.Length) % characters.Length;
-        UpdateSliders();
+        // Volta para o personagem anterior se não for o primeiro
+        if (currentCharacterIndex > 0)
+        {
+            currentCharacterIndex--;
+            UpdateSliders();
+        }
     }
 
     private void UpdateSliders()
